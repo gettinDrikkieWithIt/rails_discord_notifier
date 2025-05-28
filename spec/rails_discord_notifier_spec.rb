c1ebdbd98dc090ddc6ad6c2e99febf5f109ae698
GettinDrikkieWithIt <33983365+gettinDrikkieWithIt@users.noreply.github.com>
2025-05-28 10:11:52 +0200

    rspec test updated

--- spec/rails_discord_notifier_spec.rb
+++ spec/rails_discord_notifier_spec.rb
@@ -1,11 +1,28 @@
-# frozen_string_literal: true
+# spec/rails_discord_notifier_spec.rb
+require "spec_helper"
 
 RSpec.describe RailsDiscordNotifier do
   it "has a version number" do
-    expect(RailsDiscordNotifier::VERSION).not_to be nil
+    expect(RailsDiscordNotifier::VERSION).not_to be_nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe ".configure" do
+    after { RailsDiscordNotifier.webhook_url = nil }
+
+    it "yields the config object" do
+      called = false
+      RailsDiscordNotifier.configure do |config|
+        called = true
+        config.webhook_url = "https://discord.test/webhook"
+      end
+      expect(called).to be true
+      expect(RailsDiscordNotifier.webhook_url).to eq("https://discord.test/webhook")
+    end
+
+    it "raises if webhook_url is not set" do
+      expect {
+        RailsDiscordNotifier.configure { |c| }
+      }.to raise_error(ArgumentError, /webhook_url is required/)
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,16 @@
-# frozen_string_literal: true
-
+# spec/spec_helper.rb
+require "bundler/setup"
 require "rails_discord_notifier"
+require "logger"
+
+# Minimal Rails stub so middleware can call Rails.logger
+module Rails
+  def self.logger
+    @logger ||= Logger.new(StringIO.new)  # or STDOUT if you prefer
+  end
+end
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
-
-  # Disable RSpec exposing methods globally on `Module` and `main`
-  config.disable_monkey_patching!
-
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
