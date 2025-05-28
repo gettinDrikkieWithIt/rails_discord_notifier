18286c36b9d99dd50dc1db6693037540b150d4c9
GettinDrikkieWithIt <33983365+gettinDrikkieWithIt@users.noreply.github.com>
2025-05-28 09:18:57 +0200

    middleware added to rails_discord_notifier.rb

--- lib/rails_discord_notifier.rb
+++ lib/rails_discord_notifier.rb
@@ -1,11 +1,16 @@
+# lib/rails_discord_notifier.rb
+# frozen_string_literal: true
+
 require "rails_discord_notifier/version"
+require "rails_discord_notifier/middleware"
 require "rails_discord_notifier/railtie" if defined?(Rails)
 
 module RailsDiscordNotifier
   class << self
+    # webhook_url (String) incoming Discord webhook URL
     attr_accessor :webhook_url, :username, :avatar_url
 
-    # User calls this in an initializer
+    # Configure via initializer: RailsDiscordNotifier.configure do |config| ... end
     def configure
       yield self
       raise ArgumentError, "webhook_url is required" unless webhook_url
